c37a21417ff59f213a10bd832bafb66a4c80adb2
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-07-23 00:30:47 +0530

    new Topics

---
+++ Symbol/symbol.js
@@ -0,0 +1,35 @@
+// symbol uniquely identifies our value
+// let id1 = Symbol("id");
+// let id2 = Symbol("id");
+
+
+// alert(id1 == id2);
+
+
+// alert doesnt convert symbol into string
+// let id = Symbol("id");
+// alert(id); 
+
+
+
+// let user = {
+//     "name" :"John",
+// }
+// console.log(user);
+// let id = Symbol("id");
+// user[id] = 1;
+// alert(user[id]);
+
+
+
+
+// let id = Symbol("id");
+// let user = {
+//   name: "John",
+//   age: 30,
+//   [id]: 123
+// };
+
+// for (let key in user) alert(key);
+
+// alert("Direct : " + user[id]);
---
+++ ThisKeyword/this.js
@@ -0,0 +1,37 @@
+// let user = {
+//     "name" : "Dhruv sharma",
+//     "age" : 21,
+//     sayHi(){
+//         alert(user.name);
+//     }
+// }
+// console.log(user);
+// user.sayHi();
+
+
+
+// in this we use this keyword which points current value 
+// let user = {"name" : "Dhruv"};
+// let admin = {"name" : "Admin"};
+
+// function sayHi(){
+//     alert(this.name);
+// }
+
+// user.f = sayHi;
+// admin.f = sayHi;
+
+// user.f();
+// admin.f();
+
+// admin['f']();
+
+
+
+
+// Calling without an object: this == undefined
+// function sayHi() {
+//   alert(this);
+// }
+
+// sayHi();
