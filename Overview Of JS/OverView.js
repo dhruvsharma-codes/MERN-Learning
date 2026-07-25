131b6dabf4c8da3d0fc7e1a6a67dabb815013501
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-07-25 23:07:25 +0530

    JS Overview

---
+++ Overview Of JS/OverView.js
@@ -0,0 +1,28 @@
+/* INTRO OF JS
+
+* JavaScript was initially created to “make web pages alive”. 
+* The programs in this language are called scripts.
+* Today, JavaScript can execute not only in the browser, but also on the server, or actually on any device that has a special program called "the JavaScript engine".
+
+*/
+
+
+
+/* HOW DO ENGINES WORK
+
+Engines are complicated. But the basics are easy.
+* The engine (embedded if it’s a browser) reads the script.
+* Then it converts the script to machine code.
+* And then the machine code runs, pretty fast.
+
+ */
+
+
+
+/* WHAT MAKES JS UNIQUE
+
+* Full integration with HTML/CSS.
+* Simple things are done simply.
+* Supported by all major browsers and enabled by default.
+
+ */
