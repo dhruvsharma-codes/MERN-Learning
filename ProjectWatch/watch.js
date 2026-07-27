422849eab1e5498866a9cebfdd4665acfcde5e67
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-07-27 19:50:02 +0530

    JS Fundamentals / Watch Project

---
+++ ProjectWatch/watch.js
@@ -0,0 +1,19 @@
+
+// let timer;
+
+// function startClock(){
+//     if(!timer){
+//         timer = setInterval(showTimer,1000);
+//     }
+// }
+
+// function showTimer(){
+
+//     const date = new Date();
+//     document.getElementById("demo").innerHTML = date.toLocaleTimeString();
+// }
+
+// function stopClock(){
+// clearInterval(timer);
+// timer = undefined;
+// }
