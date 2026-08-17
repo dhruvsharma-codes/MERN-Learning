0437f9f06c9456ad70cb76c81ffb2f1a9b6e03b4
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-08-17 12:58:14 +0530

    BackendCrud

--- BACKENDJS/Backend_Sql_CRUD_5/index.js
+++ BACKENDJS/Backend_Sql_CRUD_5/index.js
@@ -4,7 +4,6 @@ const sequelize = require("./config/database.js");
 const router = require("./routes/userRoute.js");
 require("./models/userModel.js");
 const app = express();
-
 app.use(express.json());
 
 const PORT = process.env.PORT;
