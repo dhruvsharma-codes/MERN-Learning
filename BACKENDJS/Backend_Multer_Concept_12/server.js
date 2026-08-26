002363e9c7ba02a82fa7d5eb38e926eed9a5bfd2
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-08-26 15:27:00 +0530

    Updates

--- BACKENDJS/Backend_Multer_Concept_12/server.js
+++ BACKENDJS/Backend_Multer_Concept_12/server.js
@@ -3,7 +3,7 @@ const router = require("./router/multerRouter.js");
 const app = express();
 app.use(express.json());
 
-
+// routes for multer
 app.use("/multer",router);
 
 app.listen(5000, () => {
