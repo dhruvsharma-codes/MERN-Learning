2e758ab8de783293136953a3afe86c0ca2102cfb
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-16 15:28:36 +0530

    updates

--- BACKENDJS/Backend_Login_Signup_Api_9/server.js
+++ BACKENDJS/Backend_Login_Signup_Api_9/server.js
@@ -7,6 +7,12 @@ const PORT = process.env.PORT;
 
 
 app.use("/api",router);
+app.use("/api",router);
+app.use("/api",router);
+app.use("/api",router);
+app.use("/api",router);
+app.use("/api",router);
+app.use("/api",router);
 
 
 app.listen(PORT, () => {
