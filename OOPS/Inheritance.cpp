9f0fe40fd15d26577a4af968327dc036910e2b27
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-20 15:12:45 +0530

    updates

---
+++ OOPS/Inheritance.cpp
@@ -0,0 +1,36 @@
+/*
+Inheritance allows one class to acquire properties and functions of another class.
+
+eg; Animal Class
+*/
+
+#include <iostream>
+using namespace std;
+
+class Animal {
+    public:
+
+    void eat(){
+        cout << "Animal is eating." << endl;
+    }
+
+    void sleep(){
+        cout << "Animal is sleeping." << endl;
+    }
+};
+
+class Dog : public Animal {
+    public:
+
+    void bark(){
+        cout << "Dog is barking." << endl;
+    }
+};
+
+int main(){
+    Dog d;
+    d.eat();
+    d.sleep();
+    d.bark();
+    return 0;
+}
