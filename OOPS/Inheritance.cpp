da2dc815b59ca76158bc5a1274c54a489be5c31f
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-20 16:02:01 +0530

    updates

--- OOPS/Inheritance.cpp
+++ OOPS/Inheritance.cpp
@@ -39,32 +39,108 @@ eg; Animal Class
 //     return 0;
 // }
 
-
 // Single Inheritance
+// #include <iostream>
+// using namespace std;
+
+// class Animal
+// {
+// public:
+//     void eat()
+//     {
+//         cout << "Animal is eating." << endl;
+//     }
+// };
+// class Dog : public Animal
+// {
+// public:
+//     void bark()
+//     {
+//         cout << "Dog is barking." << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Dog d;
+//     d.eat();
+//     d.bark();
+//     return 0;
+// }
+
+// Multilevel Inheritnce
+// #include <iostream>
+// using namespace std;
+
+// class Animal
+// {
+// public:
+//     void eat()
+//     {
+//         cout << "Animal is eating." << endl;
+//     }
+// };
+// class Dog : public Animal
+// {
+// public:
+//     void bark()
+//     {
+//         cout << "Dog is barking." << endl;
+//     }
+// };
+// class Puppy : public Dog
+// {
+// public:
+//     void play()
+//     {
+//         cout << "Puppy is playing." << endl;
+//     }
+// };
+
+// int main()
+// {
+//     Puppy p;
+//     p.eat();
+//     p.bark();
+//     p.play();
+//     return 0;
+// }
+
+// Multiple Inheritance
 #include <iostream>
 using namespace std;
 
-class Animal
+class Father
 {
 public:
-    void eat()
+    void fatherProprty()
     {
-        cout << "Animal is eating." << endl;
+        cout << "Father Property" << endl;
     }
 };
-class Dog : public Animal
+class Mother
 {
 public:
-    void bark()
+    void motherProprty()
     {
-        cout << "Dog is barking." << endl;
+        cout << "Mother Property" << endl;
+    }
+};
+
+class Child : public Father, public Mother
+{
+public:
+    void childProperty()
+    {
+        cout << "Child Property" << endl;
     }
 };
 
 int main()
 {
-    Dog d;
-    d.eat();
-    d.bark();
+    Child c;
+    c.fatherProprty();
+    c.motherProprty();
+    c.childProperty();
     return 0;
 }
