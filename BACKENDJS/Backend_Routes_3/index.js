dcdf2b7c1ad358bfd1235c68a506fc870a0ca341
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-22 00:10:50 +0530

    updates

--- BACKENDJS/Backend_Routes_3/index.js
+++ BACKENDJS/Backend_Routes_3/index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api", userRouter);
+app.use("/api", userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is Running on PORT ${PORT}`);
