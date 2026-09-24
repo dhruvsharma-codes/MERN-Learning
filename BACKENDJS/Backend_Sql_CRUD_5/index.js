392aeb88f56282cae484489b39b934155c2f4b78
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-24 23:28:49 +0530

    updates

--- BACKENDJS/Backend_Sql_CRUD_5/index.js
+++ BACKENDJS/Backend_Sql_CRUD_5/index.js
@@ -9,6 +9,13 @@ app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
+app.use("/api", router);
 
 const startServer = async () => {
   try {
