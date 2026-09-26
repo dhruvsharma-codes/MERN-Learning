2375838c6b994c9ae7ec04d95722be22e1aef8ed
dhruvsharma <dhruv12sharma6454@gmail.com>
2026-09-26 13:53:23 +0530

    latest updates

---
+++ OOPS/polymorphism.cpp
@@ -0,0 +1,61 @@
+/*
+Polymorphism means many form. polymorphism means one interface can have many forms and behavior.
+*/
+
+// Funaction Overloading
+// #include <iostream>
+// using namespace std;
+
+// class Calculator
+// {
+// public:
+//     int add(int a, int b)
+//     {
+//         return a + b;
+//     }
+
+//     int add(int a, int b, int c)
+//     {
+//         return a + b + c;
+//     }
+// };
+
+// int main()
+// {
+//     Calculator c;
+//     cout << c.add(10, 20) << endl;
+//     cout << c.add(10, 20, 30) << endl;
+//     return 0;
+// }
+
+// function overriding
+
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void sound()
+    {
+        cout << "Animal Sound" << endl;
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    void sound() override
+    {
+        cout << "Dog barks" << endl;
+    }
+};
+
+int main()
+{
+    Animal *animal;
+    Dog dog;
+    animal = &dog;
+    animal->sound();
+    return 0;
+}
